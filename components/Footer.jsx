0f140eff742a60c65b8aa8625d35b3aa4fd4ed05
rabia zia nezami <rabianezami786@gmail.com>
2026-05-29 22:43:26 +0430

    feat: add Footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex bg-gray-800 p-4 mt-auto">
+           <p className="text-white text-center w-full">© 2024 Country Explorer. All rights reserved.</p>
+        </footer>
+    )
+}
