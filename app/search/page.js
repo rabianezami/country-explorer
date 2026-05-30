d686223e84541fe68ffb6d8b902a3a06a2698273
rabia zia nezami <rabianezami786@gmail.com>
2026-05-30 22:17:29 +0430

    feat: add SearchCountry page

--- app/search/page.js
+++ app/search/page.js
@@ -1,1 +1,14 @@
 
+import CountrySearch from "@/components/CountrySearch";
+
+export default function SearchPage() {
+  return (
+    <main className="container mx-auto px-4 py-10">
+      <h1 className="mb-8 text-center text-4xl font-bold">
+        Search Countries
+      </h1>
+
+      <CountrySearch />
+    </main>
+  );
+}
